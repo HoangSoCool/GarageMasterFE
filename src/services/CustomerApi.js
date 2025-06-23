9fe1b23dc55fe0ace2f2f9e537ed56b0a70c6e9a
HoangSoCool <hoangnguyntb@gmail.com>
2025-06-23 17:34:15 +0700

    customer 0 s

--- src/services/CustomerApi.js
+++ src/services/CustomerApi.js
@@ -1,5 +1,5 @@
 export const  API_BASE_URL = import.meta.env.VITE_API_URL  + "/api/auth";
-const API_URL = `${API_BASE_URL}/customers`;
+const API_URL = `${API_BASE_URL}/customer`;
 
 // Lấy tất cả khách hàng
 export async function getAllCustomers() {
